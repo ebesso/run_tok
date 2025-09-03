e852ece2d206841e1ee5893f1c34558307387236
ebesso <erik.besso@pihr.com>
2025-09-03 22:19:34 +0200

    Hello World API

---
+++ build/CMakeFiles/crud-exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crud-exe.dir/src/App.cpp.o"
+  "CMakeFiles/crud-exe.dir/src/App.cpp.o.d"
+  "crud-exe"
+  "crud-exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crud-exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/crud-exe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for crud-exe.
+# This may be replaced when dependencies are built.
