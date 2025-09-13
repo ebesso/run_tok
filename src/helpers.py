f184bcb74d6b59639ea8bef91953db078adcca71
ebesso <erikbesso@gmail.com>
2025-09-13 16:39:06 +0200

    Loaded and processed graph data

--- src/helpers.py
+++ src/helpers.py
@@ -1,29 +1,22 @@
 import math
 
+
 def bounding_box(lat, lon, dist_km):
-    """
-    Returns the bounding box (min_lat, min_lon, max_lat, max_lon) around a point
-    with side lengths of dist_km (in kilometers).
-    """
-    # Earth's radius in kilometers
-    R = 6371.0
+    km_per_deg_lat = 111.32
 
-    # Convert latitude and longitude from degrees to radians
-    lat_rad = math.radians(lat)
-    lon_rad = math.radians(lon)
+    half_side_km = dist_km / 2.0
+    
+    delta_lat = half_side_km / km_per_deg_lat
+    delta_lon = half_side_km / (km_per_deg_lat * math.cos(math.radians(lat)))
+    
+    nw = (lat + delta_lat, lon - delta_lon)
+    ne = (lat + delta_lat, lon + delta_lon)
+    sw = (lat - delta_lat, lon - delta_lon)
+    se = (lat - delta_lat, lon + delta_lon)
 
-    # Half side in km
-    half_side = dist_km / 2.0
-
-    # Latitude: 1 deg = 110.574 km
-    delta_lat = half_side / 110.574
-
-    # Longitude: 1 deg = 111.320*cos(latitude) km
-    delta_lon = half_side / (111.320 * math.cos(lat_rad))
-
-    min_lat = lat - delta_lat
     max_lat = lat + delta_lat
-    min_lon = lon - delta_lon
+    min_lat = lat - delta_lat
     max_lon = lon + delta_lon
+    min_lon = lon - delta_lon
 
-    return (min_lat, min_lon, max_lat, max_lon)
+    return {"nw": nw, "ne": ne, "sw": sw, "se": se, "min_lat": min_lat, "max_lat": max_lat, "min_lon": min_lon, "max_lon": max_lon}
